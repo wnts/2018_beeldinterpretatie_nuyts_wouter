ac6409961ec8b712ff098f445c3cc72a4c08579f
Wouter Nuyts <wouternuyts@gmail.com>
2018-11-28 20:53:22 +0100

    Sessie_4 cleanup en update .gitignore

--- sessie_4/main.cpp
+++ sessie_4/main.cpp
@@ -110,71 +110,70 @@ int main(int argc, char * argv[])
 
     double max_dist = 0; double min_dist = 1000.0;
     Mat img_matches;
-    do {
-		/// Minimum en maximum afstand tussen matches berekenen
-		for( int i = 0; i < matches.size(); i++ )
+
+	/// Minimum en maximum afstand tussen matches berekenen
+	for( int i = 0; i < matches.size(); i++ )
+	{
+		double dist = matches[i].distance;
+		if( dist < min_dist ) min_dist = dist;
+		if( dist > max_dist ) max_dist = dist;
+	}
+
+	fprintf(stderr, "Min distance: %f\n", min_dist);
+	fprintf(stderr, "Max distance: %f\n", max_dist);
+	/// enkel goede matches selecteren (dist <= threshold)
+	vector<DMatch> good_matches;
+	float dist_threshold = 3*min_dist;
+	fprintf(stderr, "Started with %d matches\n", matches.size());
+	for(int i = 0; i < matches.size(); i++)
+	{
+		if(matches[i].distance <= dist_threshold)
 		{
-			double dist = matches[i].distance;
-			if( dist < min_dist ) min_dist = dist;
-			if( dist > max_dist ) max_dist = dist;
+			good_matches.push_back(matches[i]);
+			// verwijder deze match ook uit de matches vector voor de volgende iteratie van de do-while loop
+			matches.erase(matches.begin() + i);
+			i++;
 		}
+	}
+	fprintf(stderr, "%d matches remaining\n", matches.size());
 
-		fprintf(stderr, "Min distance: %f\n", min_dist);
-		fprintf(stderr, "Max distance: %f\n", max_dist);
-		/// enkel goede matches selecteren (dist <= threshold)
-		vector<DMatch> good_matches;
-		float dist_threshold = 3*min_dist;
-		fprintf(stderr, "Started with %d matches\n", matches.size());
-		for(int i = 0; i < matches.size(); i++)
-		{
-			if(matches[i].distance <= dist_threshold)
-			{
-				good_matches.push_back(matches[i]);
-				// verwijder deze match ook uit de matches vector voor de volgende iteratie van de do-while loop
-				matches.erase(matches.begin() + i);
-				i++;
-			}
-		}
-		fprintf(stderr, "%d matches remaining\n", matches.size());
+	/// matches tekenen
+	drawMatches( img_input_template, keypoints_orb_template, img_input_scene, keypoints_orb_scene,
+				 good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+				 std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
 
-		/// matches tekenen
-		drawMatches( img_input_template, keypoints_orb_template, img_input_scene, keypoints_orb_scene,
-					 good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
-					 std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-
-		imshow("ORB brute force matches", img_matches);
+	imshow("ORB brute force matches", img_matches);
 
 
 
 
-		/** Object detectie mbv RANSAC/homografie **/
-		std::vector<Point2f> tpl;
-		std::vector<Point2f> scene;
-		/// keypoint coordinaten van de good_matches ophalen
-		for( size_t i = 0; i < good_matches.size(); i++ )
-		{
-			tpl.push_back( keypoints_orb_template[ good_matches[i].queryIdx ].pt );
-			scene.push_back( keypoints_orb_scene[ good_matches[i].trainIdx ].pt );
-		}
-		Mat H = findHomography( tpl, scene, RANSAC );
-		/// Coordinaten van hoekpunten template
-		std::vector<Point2f> tpl_corners(4);
-		tpl_corners[0] = cvPoint(0,0);
-		tpl_corners[1] = cvPoint(img_input_template.cols, 0);
-		tpl_corners[2] = cvPoint(img_input_template.cols, img_input_template.rows);
-		tpl_corners[3] = cvPoint(0, img_input_template.rows);
-		std::vector<Point2f> scene_corners(4);
-		/// transformeer coordinaten van hoekpunten template volgens de gevonden homografie
-		perspectiveTransform( tpl_corners, scene_corners, H);
-		/// Tekenen lijnen tussen getransformeerde hoekpunten
-		line( img_matches, scene_corners[0] + Point2f( img_input_template.cols, 0), scene_corners[1] + Point2f( img_input_template.cols, 0), Scalar(0, 255, 0), 4 );
-		line( img_matches, scene_corners[1] + Point2f( img_input_template.cols, 0), scene_corners[2] + Point2f( img_input_template.cols, 0), Scalar( 0, 255, 0), 4 );
-		line( img_matches, scene_corners[2] + Point2f( img_input_template.cols, 0), scene_corners[3] + Point2f( img_input_template.cols, 0), Scalar( 0, 255, 0), 4 );
-		line( img_matches, scene_corners[3] + Point2f( img_input_template.cols, 0), scene_corners[0] + Point2f( img_input_template.cols, 0), Scalar( 0, 255, 0), 4 );
-    }
-	while (min_dist < 100.0);
+	/** Object detectie mbv RANSAC/homografie **/
+	std::vector<Point2f> tpl;
+	std::vector<Point2f> scene;
+	/// keypoint coordinaten van de good_matches ophalen
+	for( size_t i = 0; i < good_matches.size(); i++ )
+	{
+		tpl.push_back( keypoints_orb_template[ good_matches[i].queryIdx ].pt );
+		scene.push_back( keypoints_orb_scene[ good_matches[i].trainIdx ].pt );
+	}
+	Mat H = findHomography( tpl, scene, RANSAC );
+	/// Coordinaten van hoekpunten template
+	std::vector<Point2f> tpl_corners(4);
+	tpl_corners[0] = cvPoint(0,0);
+	tpl_corners[1] = cvPoint(img_input_template.cols, 0);
+	tpl_corners[2] = cvPoint(img_input_template.cols, img_input_template.rows);
+	tpl_corners[3] = cvPoint(0, img_input_template.rows);
+	std::vector<Point2f> scene_corners(4);
+	/// transformeer coordinaten van hoekpunten template volgens de gevonden homografie
+	perspectiveTransform( tpl_corners, scene_corners, H);
+	/// Tekenen lijnen tussen getransformeerde hoekpunten
+	line( img_matches, scene_corners[0] + Point2f( img_input_template.cols, 0), scene_corners[1] + Point2f( img_input_template.cols, 0), Scalar(0, 255, 0), 4 );
+	line( img_matches, scene_corners[1] + Point2f( img_input_template.cols, 0), scene_corners[2] + Point2f( img_input_template.cols, 0), Scalar( 0, 255, 0), 4 );
+	line( img_matches, scene_corners[2] + Point2f( img_input_template.cols, 0), scene_corners[3] + Point2f( img_input_template.cols, 0), Scalar( 0, 255, 0), 4 );
+	line( img_matches, scene_corners[3] + Point2f( img_input_template.cols, 0), scene_corners[0] + Point2f( img_input_template.cols, 0), Scalar( 0, 255, 0), 4 );
 
-    fprintf(stderr, "%f\n", min_dist);
+
+
 
 
 
